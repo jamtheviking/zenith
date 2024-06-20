f668afcc39b251a86bdb4645cc478cf73cf7cb25
Jamil Bautista <jamilbautista1995@gmail.com>
2024-06-20 16:46:24 -0700

    menu trial

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.csis4175.zenith"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true;
+    }
+
     defaultConfig {
         applicationId = "com.csis4175.zenith"
         minSdk = 28
